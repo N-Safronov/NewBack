8d922449404a93c1518cb9b801f4d35eaf731c40
Nikita <ProgOutLaw@yandex.ru>
2023-06-14 19:13:43 +0300

    chore: create struct for project

---
+++ new/src/main/java/com/example/demo/entity/Tags.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tags")
+@Data
+public class Tags {
+}
